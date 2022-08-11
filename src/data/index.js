16a82ce9bfda1f3397483a67323484f656b305c8
Sid14-hub <62127472+Sid14-hub@users.noreply.github.com>
2022-08-11 18:22:20 +0530

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,53 @@
+const employeesData = [
+    {
+      id: 1,
+      firstName: 'Suraj',
+      lastName: 'chand',
+      email: 'suraj@example.com',
+      mbl:987654321
+    },
+    {
+      id: 2,
+      firstName: 'Ravi',
+      lastName: 'raj',
+      email: 'Ravi@example.com',
+      mbl:987654321
+    },
+    {
+      id: 3,
+      firstName: 'Ritik',
+      lastName: 'Sharama',
+      email: 'ritik@example.com',
+      mbl:987654321
+    },
+    {
+      id: 4,
+      firstName: 'Kesar',
+      lastName: 'kumar',
+      email: 'kesar@example.com',
+      mbl:987654321
+    },
+    {
+      id: 5,
+      firstName: 'Samridhi',
+      lastName: 'thakur',
+      email: 'thakur@example.com',
+      mbl:987654321
+    },
+    {
+      id: 6,
+      firstName: 'Roshni',
+      lastName: 'bhatt',
+      email: 'Roshni@example.com',
+      mbl:987654321
+    },
+    {
+      id: 7,
+      firstName: 'Aditya',
+      lastName: 'dj',
+      email: 'Aditya@example.com',
+      mbl:987654321
+    }
+  ];
+  
+  export { employeesData };
